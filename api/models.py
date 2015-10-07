a10e986d774ec1edaeb635b1f3f322a21a5131bd
Hao Liu <hao@Haos-MacBook-Pro.local>
2015-10-07 13:13:12 -0400

    up

--- api/models.py
+++ api/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Hot_area(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    other_name = models.TextField(blank=True)
 
     def __unicode__(self):
         return unicode(self.name)
@@ -21,6 +22,7 @@ class State(models.Model):
 
 class City(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    other_name = models.TextField(blank=True)
     state = models.ForeignKey(State)
 
     def __unicode__(self):
@@ -29,6 +31,7 @@ class City(models.Model):
 @python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    other_name = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
--- wechat/views.py
+++ wechat/views.py
@@ -11,56 +11,88 @@ from django.db.models import Q
 
 @csrf_exempt
 def wechat(request):
-    content = ''
-    post = None
+    content = None
     template = 'text.xml'
+    context = {}
 
     root = ET.fromstring(request.body)    
     msg_type = root.find('MsgType').text
     from_user = root.find('FromUserName').text
-    if msg_type == 'text':
-        msg = root.find('Content').text.encode('utf-8')
 
-        # get or create a new user
-        try:
-            user = Wechat_user.objects.get(wechat_user=from_user)
-        except:
-            user = Wechat_user(wechat_user=from_user)
-            user.save()
+    context['to'] = from_user
 
-        # create a new session or get an existing one
-        try:
-            session = Wechat_session.objects.filter(wechat_user=user).latest()
-            diff = datetime.now() - session.datetime
-            if diff.minutes > 10:
-                # start a new session
-                session = Wechat_session(wechat_user=user)
-        except:
-            session = Wechat_session(wechat_user=user)
-        # save session or refresh session 
-        session.save()
+    # get or create a new user
+    try:
+        user = Wechat_user.objects.get(wechat_user=from_user)
+    except:
+        user = Wechat_user(wechat_user=from_user)
+        user.save()
 
-        if not user.hot_area:
-            content = '想看到最新鲜，最真实，最准确的美食信息么，您现在在哪里？（目前寻味支持“纽约”和“湾区”）'
-            if msg == '纽约' or msg == '湾区':
-                user.hot_area = Hot_area.objects.get(name=msg)
-                user.save()
-                content = '您想吃点什么？（中餐/火锅/烧烤/日料/川菜...）'
+    # create a new session or get an existing one
+    try:
+        session = Wechat_session.objects.filter(wechat_user=user).latest()
+        diff = datetime.now() - session.datetime
+        if diff.minutes > 10:
+            session = None
         else:
-            post = Post.objects.filter(is_approved=True, business__hot_area=user.hot_area).filter(Q(title__icontains=msg) | Q(business__tag__name__icontains=msg) | Q(business__name__icontains=msg) | Q(business__name2__icontains=msg)).order_by('?')[:5]
-            if post:
-                template = 'news.xml'
-            else:
-                content = '很抱歉，找不到任何关于“%s”的内容'%msg
+            session.save()
+    except:
+        session = None
 
+    if not session:
+        session = Wechat_session(wechat_user=user)
+        session.save()
+        
+    if msg_type == 'text':
+        # get text message
+        msg = root.find('Content').text.encode('utf-8')
+        context = check_text(user, msg, context, template)
 
     # not support yet
     else:
-        content = '很抱歉，寻味目前还不支持这种格式。'
+        context['content'] = '很抱歉，寻味目前还不支持这种格式。'
     
-    context = {
-        'to': from_user,
-        'content': content,
-        'post': post,
-    }
     return render(request, template, context, content_type="application/xhtml+xml")
+
+def check_text(user, msg, context, template):
+    # check hot_area
+    hot_area = Hot_area.objects.get(Q(name__icontains=msg) | Q(other_name__icontains=msg))
+    if hot_area:
+        user.hot_area = hot_area
+        user.save()
+        context['content'] = '您想吃点什么？（川菜、火锅、烧烤、米其林...）'
+        return context
+    # check postcode
+    try:
+        msg = int(msg)
+    except:
+        pass
+    if isinstance(msg, int) and len(msg) == 5:
+        post = Post.objects.filter(is_approved=True, business__postcode__number=msg)
+        if post:
+            context['post'] = post
+            template = 'news.xml'
+        else:
+            context['content'] = '很抱歉，找不到位于邮编%s的餐馆'%msg
+        return context
+    # check if user has a location selected
+    if not user.hot_area:
+        context['content'] = '你想看哪里美食信息？（目前寻味支持纽约和湾区，其他北美地区敬请期待! ^_^）'
+        return context
+    # check if tag
+    post = Post.objects.filter(is_approved=True, business__hot_area=user.hot_area).filter(
+            Q(title__icontains=msg) | 
+            Q(business__tag__other_name__icontains=msg) |
+            Q(business__tag__name__icontains=msg) | 
+            Q(business__city__name__icontains=msg) |
+            Q(business__city__other_name__icontains=msg) |
+            Q(business__name__icontains=msg) | 
+            Q(business__name2__icontains=msg)
+        ).order_by('?')[:5]
+        if post:
+            context['post'] = post
+            template = 'news.xml'
+            return context
+    # fallback
+    context['content'] = '很抱歉，找不到任何关于“%s“的内容。请输入地区、菜色或者邮编给寻味，我们会帮你找到最新鲜、丰富的美食信息！'%msg
+    return context
