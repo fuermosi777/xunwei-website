01146fd950ee5c6f4de5dc084e0c580fede15edf
Hao Liu <hao@HaodeMacBook-Pro.local>
2015-10-06 21:06:27 -0400

    we test

--- wechat/views.py
+++ wechat/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def wechat(request):
     res = request.body
     return HttpResponse('success')
