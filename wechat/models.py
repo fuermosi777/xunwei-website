5fc5a0ca0620a48808f46c5e85a812fca5070d84
Hao Liu <hao@HaodeMacBook-Pro.local>
2015-10-06 23:16:51 -0400

    we test

--- wechat/models.py
+++ wechat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from api.models import *
 
 # Create your models here.
+class Wechat_user(models.Model):
+    wechat_user = models.CharField(max_length=28)
+    hot_area = model.ForeignKey(Hot_area)
+
+    def __unicode__(self):
+        return unicode(self.wechat_user)
+
+class Wechat_session(models.Model):
+    wechat_user = models.ForeignKey(Wechat_user)
+    datetime = models.DateTimeField(auto_now_add=True)
+    query = models.TextField()
--- wechat/views.py
+++ wechat/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
 import xml.etree.ElementTree as ET
+from wechat.models import *
+from datetime import datetime
 
 @csrf_exempt
 def wechat(request):
@@ -15,9 +17,32 @@ def wechat(request):
     from_user = root.find('FromUserName').text
     if msg_type == 'text':
         msg = root.find('Content').text.encode('utf-8')
-        content = '你好'
-        if '漂亮' in msg:
-            content = '我家宝宝'
+
+        # get or create a new user
+        try:
+            user = Wechat_user.get(wechat_user=from_user)
+        except:
+            user = Wechat_user(wechat_user=from_user)
+            user.save()
+
+        # create a new session or get an existing one
+        try:
+            session = Session.objects.filter(wechat_user=user).latest()
+            diff = datetime.now() - session.datetime
+            if diff.minutes > 10:
+                session = None
+                session = Session(wechat_user=user)
+        except:
+            session = Session(wechat_user=user)
+        # save session or refresh session 
+        session.save()
+
+        if not user.hot_area:
+            content = '想看到最新鲜，最真实，最准确的美食信息么，您现在在哪里？（目前寻味支持“纽约”和“湾区”）'
+        else:
+            content = '知道你的位置了，然后呢？'
+
+    # not support yet
     else:
         content = '很抱歉，寻味目前还不支持这种格式。'
     
