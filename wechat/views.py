7bb5643383eb673d9ed692cef9f8d7382ad8bbda
Hao Liu <hao@HaodeMacBook-Pro.local>
2015-10-06 21:15:42 -0400

    we test

--- wechat/views.py
+++ wechat/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
@@ -6,4 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def wechat(request):
     res = request.body
-    return HttpResponse('success')
+    context = {
+        'content': u'你好'
+    }
+    return render(request, 'text.xml', context, content_type="application/xhtml+xml")
