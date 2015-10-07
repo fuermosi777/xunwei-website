b6b3378cdf0d5ffe162a5ddec1d1e4c4e88f90d3
Hao Liu <hao@HaodeMacBook-Pro.local>
2015-10-06 21:32:43 -0400

    we test

--- wechat/views.py
+++ wechat/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
 from django.views.decorators.csrf import csrf_exempt
+import xml.etree.ElementTree as ET
 
 @csrf_exempt
 def wechat(request):
-    res = request.body
+    root = ET.fromstring(request.body)
+    from_user = root.find('FromUserName').text
     context = {
-        'content': u'你好'
+        'to': from_user,
+        'content': '你好'
     }
     return render(request, 'text.xml', context, content_type="application/xhtml+xml")
