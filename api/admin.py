58141ca65b58f1395a0858477e8cc9b38f791ee5
Hao Liu <hao@HaodeMacBook-Pro.local>
2015-10-08 21:00:14 -0400

    admin

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from api.models import *
 
+class BusinessAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'name2')
+    search_fields = ['name']
+
 # Register your models here.
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Tag)
 admin.site.register(Postcode)
-admin.site.register(Business)
+admin.site.register(Business, BusinessAdmin)
 admin.site.register(Post)
 admin.site.register(Post_photo)
 admin.site.register(Hot_area)
